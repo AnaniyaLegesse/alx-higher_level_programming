c24255773208254ea9e6c610de1419fe5c7f6721
AnaniyaLegesse <ananiya.legesse1@gmail.com>
2023-07-10 18:57:21 -0700

    inherits_from bug fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""
-Contains the inherits_from function
-"""
+"""Module containing inherits_from method"""
 
 
 def inherits_from(obj, a_class):
-    """returns true if obj is a subclass of a_class, otherwise false"""
-    return(issubclass(type(obj), a_class) and type(obj) != a_class)
+    """returns True if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class;
+    otherwise False"""
+    return isinstance(obj, a_class) and type(obj) != a_class
